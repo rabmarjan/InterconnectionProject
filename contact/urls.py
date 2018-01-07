817d282a08004bc1f6a8bf8e46327de51a046021
Abdur Rab Marjan <rab.marjan@gmail.com>
2018-01-07 13:31:47 +0600

    Update urls.py
    Updated for Django 2.0

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+from django.urls import path
 from contact.views import ContactView, ContactDetail
 
 urlpatterns = [
-    url(r'^api/v1/contact/$', ContactView.as_view()),
-    url(r'^api/v1/contact/(?P<contact_id>[0-9]+)/$', ContactDetail.as_view()),
+    path('api/v1/contact/', ContactView.as_view()),
+    path('api/v1/contact/<int:id>', ContactDetail.as_view()),
 ]
