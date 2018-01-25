151244eb5303979ce4ae2947f0fb1f4a3df316e3
Abdur Rab Marjan <rab.marjan@gmail.com>
2018-01-25 07:05:09 +0000

    Update urls.py

--- InterconnectionContact/urls.py
+++ InterconnectionContact/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url, include
-from django.urls import path
-
+from django.conf.urls import url
+from django.urls import include, path
 urlpatterns = [
     path('', include('contact.urls')),
 ]
